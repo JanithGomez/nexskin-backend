de13dd7e87c50e5ec3dd360d195705bd84a246a7
Janith Gomez <janithgomezsoft@gmail.com>
2026-02-22 20:20:53 +0000

    small changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,6 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         
-         // ✅ NEW: cart group (session enabled)
         'cart' => [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
