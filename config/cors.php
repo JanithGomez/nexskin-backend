9bae29063bc24c839166c4e3483080e64fc4a39a
Janith Gomez <janithgomezsoft@gmail.com>
2026-02-23 12:46:51 +0000

    fix core.php

--- config/cors.php
+++ config/cors.php
@@ -1,6 +1,40 @@
 <?php
 
-return [
+// return [
+
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Cross-Origin Resource Sharing (CORS) Configuration
+//     |--------------------------------------------------------------------------
+//     |
+//     | Here you may configure your settings for cross-origin resource sharing
+//     | or "CORS". This determines what cross-origin operations may execute
+//     | in web browsers. You are free to adjust these settings as needed.
+//     |
+//     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//     |
+//     */
+
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => [
+//     'http://frontend.nexskin.test:3000',
+//     ],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+
+// ];
+ return [
 
     /*
     |--------------------------------------------------------------------------
@@ -15,13 +49,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'livewire/*', 'admin/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-    'http://frontend.nexskin.test:3000',
-    ],
+    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', ''))),
 
     'allowed_origins_patterns' => [],
 
